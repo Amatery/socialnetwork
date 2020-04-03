94d0fb948e82a9d805c63fbccef0339dcd1fd261
Home PC <sindzi0007@yandex.ru>
2020-04-03 18:22:31 +0300

    Added components : Header,Navbar,Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://yt3.ggpht.com/a-/AAuE7mB14E4Unb_EJsgHGROrikWLFdOS8MEYrhCtqw=s900-mo-c-c0xffffffff-rj-k-no'/>
+        </header>
+    );
+}
+
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src='https://greencode.com.ua/wp-content/uploads/2018/02/tablet_smart_phone_hi_tech_icons_hand_96123_1920x1080-min-ru-2-1200x469.jpg'/>
+            </div>
+            <div>ava+description</div>
+            <div>my posts</div>
+            <div>new post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    );
+}
+
+
+export default Profile;
